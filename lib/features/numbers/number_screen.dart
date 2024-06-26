cbac9b451fc3dee7146ce8e126e9bfd08a5e96f8
Nikolai Luft <nikoluft92@gmail.com>
2024-06-26 22:11:24 +0200

    number screen

--- lib/features/numbers/number_screen.dart
+++ lib/features/numbers/number_screen.dart
@@ -6,17 +6,37 @@ class NumberScreen extends StatelessWidget {
     super.key,
     required this.numberRepository,
   });
-
   final NumberRepository numberRepository;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Number Screen'),
       ),
-      body: const Center(
-        child: Text("Hier sollen die Zahlen stehen"),
+      body: StreamBuilder<int>(
+        stream: numberRepository.getNumberStream(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text('Fehler'),
+            );
+          } else if (!snapshot.hasData) {
+            return const Center(
+              child: Text('Keine Daten vorhanden'),
+            );
+          } else {
+            return Center(
+              child: Text(
+                'Current Number: ${snapshot.data}',
+                style: const TextStyle(fontSize: 24),
+              ),
+            );
+          }
+        },
       ),
     );
   }
